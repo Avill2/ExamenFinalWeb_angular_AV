db89b1af2e0c0a3260fffb21440c6e6e8d55686e
Avill2 <andrea.villacis@epn.edu.ec>
2018-07-12 18:32:11 -0500

    componente usuario y busqueda

---
+++ my-app/src/app/busqueda/busqueda.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {UsuariosComponent} from '../usuarios/usuarios.component';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+export class BusquedaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  UsuarioHacerClick:UsuariosComponent;
+  mostrarBusqueda=false;
+
+}
--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  title = 'Examen de web 2 Bimestre';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  email;
+  password;
+  passwordConfirmation;
+
+  ingresarUsuario(formulario) {
+
+    const valorActualPassword = formulario
+      .controls.password.value;
+
+    const valorActualPasswordConfirmation = formulario.controls.passwordConfirmation.value;
+
+    if (valorActualPassword === valorActualPasswordConfirmation) {
+      alert('BIEN!');
+    } else {
+      alert('Los passwords no son iguales');
+      this.password = undefined;
+      this.passwordConfirmation = undefined;
+    }
+
   }
 
 }
